b12781937f8f3dda694a4fb6ec5182efca9a2d12
Brett Sheffield <brett@gladserv.com>
2019-12-02 06:44:27 +0000

    fix email address

--- src/log.c
+++ src/log.c
@@ -4,7 +4,7 @@
  *
  * this file is part of LIBRESTACK
  *
- * Copyright (c) 2012-2019 Brett Sheffield <brett@gladserv.com>
+ * Copyright (c) 2012-2019 Brett Sheffield <bacs@librecast.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
