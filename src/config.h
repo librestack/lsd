47d8ab174150bbe3968b053782f2eb0bde7af5f4
Brett Sheffield <brett@gladserv.com>
2019-12-06 20:07:36 +0000

    set default loglevel to INFO

--- src/config.h
+++ src/config.h
@@ -38,7 +38,7 @@ typedef enum {
 /* key, short, long, type, var, value, helptext */
 #define CONFIG_ITEMS(X) \
 	X(filename, "-C", "--config", CONFIG_TYPE_STRING, char *, NULL, "path to config file") \
-	X(loglevel, "-l", "--loglevel", CONFIG_TYPE_INT, int, 65, "logging level") \
+	X(loglevel, "-l", "--loglevel", CONFIG_TYPE_INT, int, 15, "logging level") \
 	X(daemon, "-d", "--daemon", CONFIG_TYPE_BOOL, int, 0, "daemonize")
 #undef X
 
