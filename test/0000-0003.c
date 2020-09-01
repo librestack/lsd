64256ae0636d6b511fb49128e566026d416226d3
Brett Sheffield <brett@gladserv.com>
2020-09-01 09:35:22 +0100

    test config_init() / config_close()

---
+++ test/0000-0003.c
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright (c) 2020 Brett Sheffield <bacs@librecast.net> */
+
+#include "test.h"
+#include "../src/config.h"
+
+int main()
+{
+	char dbpath[] = "0000-0003.tmp.XXXXXX";
+	char *dbtemp = mkdtemp(dbpath);
+	char *argv[] = { "0000-0003", "--dbpath", dbtemp, NULL };
+	int argc = sizeof argv / sizeof argv[0] - 1;
+	test_name("config_init() / config_close()");
+
+	/* check for leaks - requires make check */
+	config_init(argc, argv);
+	config_close();
+
+	return fails;
+}
